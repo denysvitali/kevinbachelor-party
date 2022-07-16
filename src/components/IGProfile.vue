ff2660ca459c58295ec4c4ad87f7548143b70d94
Andrea Faggin <blackbird5308@gmail.com>
2022-07-16 10:33:56 +0200

    Fixup mobile

--- src/components/IGProfile.vue
+++ src/components/IGProfile.vue
@@ -1,39 +1,31 @@
 <script setup lang="ts">
-const props = defineProps(['username'])
-
-function navigate_profile(){
-  window.location.assign(`https://instagr.am/${props.username}`);
-}
-
-function navigate_profile_touch(e: any){
-  window.location.assign(`https://instagr.am/${props.username}`);
-}
+const props = defineProps(["username"]);
+const src = `/profiles/${props.username}.jpg`;
 </script>
 
 <template>
-  <div class="profile-container" 
-    @touchstart="navigate_profile_touch"
-    @click="navigate_profile">
-    <img :src="'/assets/profiles/' + props.username + '.jpg'"/>
+  <a class="profile-container" :href="`https://instagr.am/${props.username}`">
+    <img :src="src" />
     <p class="username">@{{ props.username }}</p>
-  </div>
+  </a>
 </template>
 
 <style scoped>
-div.profile-container {
+a.profile-container {
   display: flex;
   flex-direction: column;
   align-items: center;
   cursor: pointer;
   margin-top: 20px;
   margin: 20px 30px 0px 30px;
+  text-decoration: none;
+  color: #fff;
 }
 
-div.profile-container p {
-
+a.profile-container p {
 }
 
-div.profile-container > img {
+a.profile-container > img {
   width: 80px;
   height: 80px;
   border-radius: 80px;
--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,39 +1,49 @@
 <script setup lang="ts">
-import IGProfile from '@/components/IGProfile.vue'
+import IGProfile from "@/components/IGProfile.vue";
 </script>
 
 <template>
   <div class="faq">
     <h2>Who's getting married?</h2>
     <p>
-      Kevin and Marzia are getting married this month - we're celebrating with Kevin
-      the last days before the marriage.
+      Kevin and Marzia are getting married this month - we're celebrating with
+      Kevin the last days before the marriage.
     </p>
 
     <h2>Where are you celebrating?</h2>
     <p>
-      In Budapest! If you've seen us at the airport, Kevin still doesn't know anything about
-      what we're going to do!
+      In Budapest! If you've seen us at the airport, Kevin still doesn't know
+      anything about what we're going to do!
     </p>
 
     <h2>Who's the Groom?</h2>
     <div class="profiles-content">
-      <IGProfile username="keie_92_qv"/>
+      <IGProfile username="keie_92_qv" />
     </div>
 
     <h2>Who's the Groom Team?</h2>
     <div class="profiles-content">
-      <IGProfile :username="profile" v-for="profile in ['the_repole', 'pat__ferre', 'dellemonache.claudio', 'teo_corre', 'denvit']"/>
+      <IGProfile
+        :username="profile"
+        v-for="profile in [
+          'the_repole',
+          'pat__ferre',
+          'dellemonache.claudio',
+          'teo_corre',
+          'denvit',
+        ]"
+      />
     </div>
   </div>
 </template>
 
 <style>
 div.faq {
-    display: flex;
-    flex-direction: column;
-    align-items: top;
-    padding: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: top;
+  padding: 50px;
+  max-width: 100vw;
 }
 
 div.faq p {
@@ -44,5 +54,11 @@ div.profiles-content {
   display: flex;
   flex-direction: row;
   align-items: center;
+}
+@media all and (max-width: 767px) {
+  div.profiles-content {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 }
 </style>
